156773bdd87600216bba6799b1d809608377351b
ThiagoORuby <trs@ic.ufal.br>
2023-12-29 12:15:20 -0300

    Cria base do backend

---
+++ backend/configurations/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from settings import settings
+
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionFactory = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine)
+
+
