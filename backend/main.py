43d68426432dea017a1378a9e1397fcf530b9c1d
ThiagoORuby <trs@ic.ufal.br>
2023-12-30 16:22:55 -0300

    Adiciona lista de origens do CORS

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.api_router import api_router
 from services.db import create_default_user, engine
+from services.settings import settings
 
 app = FastAPI(title="GAP Project", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
