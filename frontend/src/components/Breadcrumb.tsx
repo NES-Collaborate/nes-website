f592dcf36a3dedbd656826822a6e39e70995ef9d
sr-pato <felipe.adeildo@proton.me>
2024-01-01 22:58:23 -0300

    Adiciona divider no breadcrumb

--- frontend/src/components/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumb.tsx
@@ -2,7 +2,7 @@ import { BREADCRUMB_NAMES } from "@/data/constants"
 import clsx from "clsx"
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
-import { Breadcrumbs } from "react-daisyui"
+import { Breadcrumbs, Divider } from "react-daisyui"
 
 export const Breadcrumb = () => {
   const pathname = usePathname()
@@ -17,23 +17,21 @@ export const Breadcrumb = () => {
   }, [render, pathname, splitedPath.length])
 
   return (
-    <div
-      className={clsx(
-        render && "flex justify-around py-1 border-b-2 mb-1 border-b-green-nes",
-        !render && "hidden"
-      )}
-    >
-      <Breadcrumbs>
-        {splitedPath.map((x, i) => {
-          const url = `/${splitedPath.slice(0, i + 1).join("/")}`
+    <>
+      <div className={clsx(render && "flex justify-around pt-1", !render && "hidden")}>
+        <Breadcrumbs>
+          {splitedPath.map((x, i) => {
+            const url = `/${splitedPath.slice(0, i + 1).join("/")}`
 
-          return (
-            <Breadcrumbs.Item key={i} href={url}>
-              {BREADCRUMB_NAMES[x] || x}
-            </Breadcrumbs.Item>
-          )
-        })}
-      </Breadcrumbs>
-    </div>
+            return (
+              <Breadcrumbs.Item key={i} href={url}>
+                {BREADCRUMB_NAMES[x] || x}
+              </Breadcrumbs.Item>
+            )
+          })}
+        </Breadcrumbs>
+      </div>
+      <Divider color="accent" className="m-0" />
+    </>
   )
 }
