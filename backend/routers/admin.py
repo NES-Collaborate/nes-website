078867851156973844c480293a4fcf1ecc37a495
ThiagoORuby <trs@ic.ufal.br>
2024-01-08 19:38:13 -0300

    Cria CRUD de Administração de Bens

---
+++ backend/routers/admin.py
@@ -0,0 +1,94 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from models.admin import Property
+from models.user import User
+from schemas.admin import PropertyIn, PropertyOut
+from services.db import get_session
+from services.user import UserService
+from sqlalchemy.orm import Session
+
+router = APIRouter(prefix="/admin", tags=["admin"])
+
+
+@router.get("/property", status_code=status.HTTP_200_OK)
+async def get_properties(
+        current_user: User = Depends(UserService.get_current_user),
+        session: Session = Depends(get_session),
+        q: str = ""):
+
+    if current_user.type != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuário não autorizado")
+
+    results = session.query(Property).all()
+
+    return [PropertyOut.model_validate(result) for result in results]
+
+
+@router.post("/property")
+async def create_property(
+        property: PropertyIn,
+        current_user: User = Depends(UserService.get_current_user),
+        session: Session = Depends(get_session),
+):
+
+    if current_user.type != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuário não autorizado")
+
+    _property = Property(**property.model_dump())
+
+    session.add(_property)
+    session.commit()
+    session.refresh(_property)
+
+    return PropertyOut.model_validate(_property)
+
+
+@router.put("/property/{property_id}")
+async def update_property(
+        property_id: int,
+        property: PropertyIn,
+        current_user: User = Depends(UserService.get_current_user),
+        session: Session = Depends(get_session),
+):
+
+    if current_user.type != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuário não autorizado")
+
+    _property = session.query(Property).get(property_id)
+
+    if not _property:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Propriedade não encontrada")
+
+    _property.name = property.name
+    _property.type = property.type
+    session.add(_property)
+    session.commit()
+    session.refresh(_property)
+
+    return PropertyOut.model_validate(_property)
+
+
+@router.delete("/property/{property_id}")
+async def delete_property(
+        property_id: int,
+        current_user: User = Depends(UserService.get_current_user),
+        session: Session = Depends(get_session),
+):
+
+    if current_user.type != "admin":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Usuário não autorizado")
+
+    _property = session.query(Property).get(property_id)
+
+    if not _property:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Propriedade não encontrada")
+
+    session.delete(_property)
+    session.commit()
+
+    return {"message": f"Propriedade com id {property_id} deletada"}
--- backend/routers/api_router.py
+++ backend/routers/api_router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from routers import auth, teacher
-
-from backend.routers import classroom
+from routers import admin, auth, classroom, teacher
 
 api_router = APIRouter(prefix="/api", tags=["api"])
 
 api_router.include_router(auth.router)
 api_router.include_router(classroom.router)
 api_router.include_router(teacher.router)
+api_router.include_router(admin.router)
