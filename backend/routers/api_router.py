14134e138616d56724ac7bbdaa06faafc530667d
ThiagoORuby <trs@ic.ufal.br>
2024-01-08 02:54:37 -0300

    Correção de nome das rotas

--- backend/routers/api_router.py
+++ backend/routers/api_router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from routers import auth, teacher
 
-from routers import auth, student, teacher
+from backend.routers import classroom
 
 api_router = APIRouter(prefix="/api", tags=["api"])
 
 api_router.include_router(auth.router)
-api_router.include_router(student.router)
+api_router.include_router(classroom.router)
 api_router.include_router(teacher.router)
