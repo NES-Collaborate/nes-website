a0ca0cacac4fa66542065650bd37660581f5e4c7
sr-pato <felipe.adeildo@proton.me>
2024-01-08 15:47:37 -0300

    Adiciona infinite scroll nas postagens para o estudante

--- frontend/src/components/app/Posts.tsx
+++ frontend/src/components/app/Posts.tsx
@@ -1,76 +1,64 @@
-import Loading from "@/components/Loading"
 import { useSession } from "@/contexts/session"
 import { Post } from "@/types/entities"
 import { axiosServer } from "@/utils/axiosClient"
 import { useEffect, useState } from "react"
-import { Alert, Pagination } from "react-daisyui" // Removed Button
+import { useInView } from "react-intersection-observer"
+import Loading from "../Loading"
 import PostCard from "./PostCard"
 
-export type PostsPagination = {
-  data: Post[]
-  nextPage?: number
-  page: number
-  prevPage?: number
-  total: number
-}
-
 const Posts = () => {
-  const [postsPagination, setPostsPagination] = useState<PostsPagination>({
-    data: [],
-    page: 1,
-    total: 0,
-  })
-  const [isLoading, setIsLoading] = useState(true)
   const session = useSession()
-  const [error, setError] = useState<string>("")
+  const [page, setPage] = useState(1)
+  const [posts, setPosts] = useState<Post[]>([])
+  const [hasNextPage, setHasNextPage] = useState(true)
+  const [isFetching, setIsFetching] = useState(false)
+  const { ref, inView } = useInView()
 
   useEffect(() => {
-    setIsLoading(true)
-    const fetchPosts = async () => {
+    const getPosts = async (page: number, perPage: number) => {
       try {
+        setIsFetching(true)
         const res = await axiosServer.get("/student/posts", {
           headers: {
             Authorization: `Bearer ${session.token}`,
           },
+          params: {
+            p: page,
+            pp: perPage,
+          },
         })
 
-        setPostsPagination(res.data as PostsPagination)
-      } catch {
-        setError("Erro ao carregar postagens. Tente novamente mais tarde.")
+        if (res.data.data.length > 0) {
+          setPosts((prevPosts) => [...prevPosts, ...res.data.data])
+          setHasNextPage((_) => res.data.nextPage || false)
+        }
+      } catch (err: unknown) {
+        console.log(err)
       } finally {
-        setIsLoading(false)
+        setIsFetching(false)
       }
     }
 
-    fetchPosts()
-  }, [setPostsPagination, session.token])
+    if (inView && hasNextPage && !isFetching) {
+      getPosts(page, 2)
+      setPage((p) => p + 1)
+    }
+  }, [inView, hasNextPage, page, session.token, isFetching])
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-center text-3xl font-semibold mt-2 mb-4">Postagens</h1>
+    <div className="flex flex-col gap-3 mx-auto">
+      <h1 className="text-3xl font-bold text-center mt-2">Postagens</h1>
+      {posts.map((post) => (
+        <PostCard post={post} key={post.id} />
+      ))}
 
-      {/* TODO: Add filters here to fetch posts */}
-
-      {/* TODO: Add infinity scroll here */}
-      {postsPagination.total > 0 && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {postsPagination.data.map((post) => (
-            <PostCard post={post} key={post.id} />
-          ))}
-        </div>
-      )}
-
-      {error && (
-        <Alert status="error" className="mt-4">
-          {error}
-        </Alert>
-      )}
-
-      {isLoading && (
-        <div className="mt-4">
-          <Loading text="Carregando..." textClassName="text-xl" center />
-        </div>
-      )}
+      <div className="my-3" ref={ref}>
+        {hasNextPage ? (
+          <Loading text="Carregando..." textClassName="text-lg" center />
+        ) : (
+          <p className="text-center text-lg">Sem mais postagens</p>
+        )}
+      </div>
     </div>
   )
 }
