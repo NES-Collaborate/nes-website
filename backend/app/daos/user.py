5b57806e3e042a9ea290b147c39ebd3f22a4fb37
sr-pato <felipe.adeildo@proton.me>
2024-01-24 23:30:23 -0300

    Corrige erro de verificação de senha (`password` pode não existir)

--- backend/app/daos/user.py
+++ backend/app/daos/user.py
@@ -8,15 +8,15 @@ from .general import GeneralDao
 
 
 class UserDao(BaseDao):
-
     def create(self, user_data: dict[str, Any]) -> User | None:
-
-        _user = User(name=user_data["name"],
-                     type=user_data["type"],
-                     cpf=user_data["cpf"],
-                     birthdate=user_data["birthdate"],
-                     scholarship=user_data["scholarship"],
-                     serie=user_data["serie"])
+        _user = User(
+            name=user_data["name"],
+            type=user_data["type"],
+            cpf=user_data["cpf"],
+            birthdate=user_data["birthdate"],
+            scholarship=user_data["scholarship"],
+            serie=user_data["serie"],
+        )
 
         _user.password = hash.bcrypt.hash(user_data["password"])
         self.session.add(_user)
@@ -30,8 +30,7 @@ class UserDao(BaseDao):
 
         GeneralDao(self.session).create_address(user_data["address"], _user.id)
 
-        GeneralDao(self.session).create_attachment(user_data["photo"],
-                                                   _user.id)
+        GeneralDao(self.session).create_attachment(user_data["photo"], _user.id)
 
         self.session.refresh(_user)
         return _user
@@ -54,17 +53,21 @@ class UserDao(BaseDao):
             GeneralDao(self.session).create_phone_number(phone, _user.id)
 
         GeneralDao(self.session).update_address(user_data["address"], _user.id)
-        GeneralDao(self.session).update_attachment(user_data["photo"],
-                                                   _user.id)
+        GeneralDao(self.session).update_attachment(user_data["photo"], _user.id)
 
         UPDATED_KEYS = [
-            "name", "type", "cpf", "birthdate", "scholarship", "serie"
+            "name",
+            "type",
+            "cpf",
+            "birthdate",
+            "scholarship",
+            "serie",
         ]
         for key, value in user_data.items():
             if key in UPDATED_KEYS:
                 setattr(_user, key, value)
 
-        if user_data["password"]:
+        if user_data.get("password"):
             _user.password = hash.bcrypt.hash(user_data["password"])
 
         self.session.commit()
@@ -72,15 +75,14 @@ class UserDao(BaseDao):
         return _user
 
     def delete_by_id(self, id: int):
-
         _user = self.get_by_id(id)
         if _user is None:
             return None
 
-        for email in _user.emails:  #type: ignore
+        for email in _user.emails:  # type: ignore
             self.session.delete(email)
 
-        for phone in _user.phones:  #type: ignore
+        for phone in _user.phones:  # type: ignore
             self.session.delete(phone)
 
         self.session.delete(_user.address)
