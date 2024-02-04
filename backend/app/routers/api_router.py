4865a74d02ec148edff7a670628a15852b061e54
ThiagoORuby <trs@ic.ufal.br>
2024-02-04 00:01:12 -0300

    Adiciona rotas refatoradas de admin

--- backend/app/routers/api_router.py
+++ backend/app/routers/api_router.py
@@ -1,10 +1,10 @@
+from app.routers import auth, classroom, teacher
+from app.routers.admin import admin_router
 from fastapi import APIRouter
-
-from app.routers import admin, auth, classroom, teacher
 
 api_router = APIRouter(prefix="/api", tags=["api"])
 
 api_router.include_router(auth.router)
 api_router.include_router(classroom.router)
 api_router.include_router(teacher.router)
-api_router.include_router(admin.router)
+api_router.include_router(admin_router.router)
