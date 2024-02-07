2ca64e33bdd0d5bf13cdc4b37488bf1d977dc298
sr-pato <oie.eu.sou.um@gmail.com>
2024-02-07 15:08:57 -0300

    Add uploadAttach function Upload an file to attachment in backend

--- frontend/src/utils/client.ts
+++ frontend/src/utils/client.ts
@@ -1,4 +1,6 @@
+import { Attach } from "@/types/entities"
 import { User } from "@/types/user"
+import { axiosServer } from "./axiosClient"
 
 /**
  * Get user photo url to display
@@ -7,3 +9,24 @@ import { User } from "@/types/user"
  */
 export const getUserPhotoUrl = (user: User) =>
   user?.photo?.location || "/img/default-user.png"
+
+/**
+ * Upload a file to backend and register it as an attachment
+ * @param attach A file to upload
+ * @param token The logged user session
+ * @returns Attach if success or error message
+ */
+export const uploadAttach = async (attach: any, token: string) => {
+  const form = new FormData()
+  form.append("data", attach)
+  try {
+    const res = await axiosServer.post("/upload", form, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.data as Attach
+  } catch {
+    return "Erro ao registrar anexo. Tente novamente mais tarde."
+  }
+}
