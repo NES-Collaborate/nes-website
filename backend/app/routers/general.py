c9a30ff61302be4d704c12324962a1fc311bd1e6
sr-pato <oie.eu.sou.um@gmail.com>
2024-02-12 01:09:54 -0300

    Adicionar API para acesso aos attachments por ID

--- backend/app/routers/general.py
+++ backend/app/routers/general.py
@@ -5,6 +5,7 @@ from app.services.db import get_session
 from app.services.settings import Settings
 from app.services.user import UserService
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -30,18 +31,46 @@ async def upload_file(
 
     _path.mkdir(parents=True, exist_ok=True)
 
+    save_path = _path / filename
+
     # TODO: Improve this code and make more secure and efficient to save the file
     # 1. The file must be saved usign async ietartor over the file chunks (1024 bytes)
     # 2. Securely rewrite the filename (especial characters using regex and more)
-    with open(_path / filename, "wb") as f:
+    with open(save_path, "wb") as f:
         f.write(await data.read())
 
-    _attach = Attach(name=data.filename,
-                     location=str(_path),
-                     type="file",
-                     user_id=current_user.id)
+    _attach = Attach(name=str(filename), location=str(save_path), type="file")
     session.add(_attach)
     session.commit()
     session.refresh(_attach)
 
     return {"id": _attach.id}
+
+
+@router.get("/attachments/{attach_id}", status_code=status.HTTP_200_OK)
+async def get_attachments(
+        attach_id: int,
+        session: Session = Depends(get_session),
+):
+
+    _attach = session.query(Attach).get(attach_id)
+
+    if not _attach:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Anexo não encontrado",
+        )
+
+    _path = Path(_attach.location)
+    if not _path.is_file():
+        _path /= _attach.name
+
+    if not _path.is_file():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Anexo não encontrado",
+        )
+
+    return FileResponse(_path,
+                        filename=_attach.name,
+                        content_disposition_type="inline")
