18c961ab659ce2cadf685d9612d019cbdc7e4f88
ThiagoORuby <trs@ic.ufal.br>
2024-02-11 22:05:46 -0300

    Adiciona constate para o caminho de uploads

--- backend/app/services/settings.py
+++ backend/app/services/settings.py
@@ -3,6 +3,7 @@ class Settings:
     DATABASE_URL: str = "sqlite:///./db.sqlite"
     SECRET_KEY: str = "something here (recommend: openssl rand -hex 32)"
     ALGORITHM: str = "HS256"
+    UPLOADS_PATH: str = "uploads"
     DEFAULT_PASS: str = "default"
     ORIGINS: list[str] = [
         "http://localhost:3000",
