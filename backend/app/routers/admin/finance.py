776961c6a57e9e455f14d1c5a80be9802d160e69
ThiagoORuby <trs@ic.ufal.br>
2024-02-11 22:04:05 -0300

    Adiciona rotas de pagamento de bolsa e filtros
    Co-authored-by: Felipe Adeildo <felipe-adeildo@users.noreply.github.com>

--- backend/app/routers/admin/finance.py
+++ backend/app/routers/admin/finance.py
@@ -1,18 +1,20 @@
 from app.daos.admin import AdminDao
-from app.models.expense import ExpenseLog
+from app.daos.user import UserDao
+from app.models.expense import ExpenseCategory, ExpenseLog
 from app.models.user import User
-from app.schemas.expense import ExpenseLogIn, ExpenseLogOut
+from app.schemas.expense import ExpenseLogIn, ExpenseLogOut, ScolarshipPayment
+from app.schemas.user import UserPayment
 from app.services.db import get_session
 from app.services.user import UserService
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, aliased
 
 router = APIRouter(prefix="/finance", tags=["finance"])
 
 
 @router.get("/stats", status_code=status.HTTP_200_OK)
-def get_stats(current_user: User = Depends(UserService.get_current_user),
-              session: Session = Depends(get_session)):
+async def get_stats(current_user: User = Depends(UserService.get_current_user),
+                    session: Session = Depends(get_session)):
 
     if current_user.type != "admin":
         raise HTTPException(
@@ -24,8 +26,14 @@ def get_stats(current_user: User = Depends(UserService.get_current_user),
 
 
 @router.get("", status_code=status.HTTP_200_OK)
-def get_finances(current_user: User = Depends(UserService.get_current_user),
-                 session: Session = Depends(get_session)):
+async def get_finances(category: str = "",
+                       addedBy: str = "",
+                       paidTo: str = "",
+                       comment: str = "",
+                       type: str = "",
+                       current_user: User = Depends(
+                           UserService.get_current_user),
+                       session: Session = Depends(get_session)):
 
     if current_user.type != "admin":
         raise HTTPException(
@@ -33,15 +41,40 @@ def get_finances(current_user: User = Depends(UserService.get_current_user),
             detail="Usuário não autorizado",
         )
 
-    results = session.query(ExpenseLog).all()
-    expenses = [ExpenseLogOut.model_validate(result) for result in results]
+    paidToUsers = aliased(User)
+
+    results = session.query(ExpenseLog) \
+        .join(ExpenseCategory, ExpenseLog.category_id == ExpenseCategory.id) \
+        .join(User, ExpenseLog.user_id == User.id)
+
+    conditions = [
+        ExpenseCategory.name.contains(category.strip()),
+        ExpenseLog.comment.contains(comment.strip()),
+        ExpenseLog.type.contains(type.strip()),
+        User.name.contains(addedBy.strip()),
+    ]
+
+    paidTo = paidTo.strip()
+    if paidTo:
+        results = results.outerjoin(paidToUsers,
+                                    paidToUsers.id == ExpenseLog.paidto_id)
+        conditions.append(paidToUsers.name.contains(paidTo.strip()))
+
+    results = results.filter(*conditions)
+
+    expenses = [
+        ExpenseLogOut.model_validate(result) for result in results.all()
+    ]
     return {"logs": expenses}
 
 
-@router.post("")
-def create_finance(expense_log: ExpenseLogIn,
-                   current_user: User = Depends(UserService.get_current_user),
-                   session: Session = Depends(get_session)):
+@router.get("/students")
+async def get_students(year: int,
+                       month: int,
+                       classroomId: int,
+                       current_user: User = Depends(
+                           UserService.get_current_user),
+                       session: Session = Depends(get_session)):
 
     if current_user.type != "admin":
         raise HTTPException(
@@ -49,7 +82,56 @@ def create_finance(expense_log: ExpenseLogIn,
             detail="Usuário não autorizado",
         )
 
-    _expense = AdminDao(session).create_expense(expense_log)
+    response: list[UserPayment] = []
+    _users = UserDao(session).get_by_classroom(classroomId)
+
+    for _user in _users:
+
+        data = UserPayment.model_validate(_user)
+
+        if AdminDao(session).get_scolarship_payment(_user.id, year, month):
+            data.already_paid = True
+        else:
+            data.already_paid = False
+
+        response.append(data)
+
+    return response
+
+
+@router.post("/students/pay")
+async def pay_students(payment: ScolarshipPayment,
+                       current_user: User = Depends(
+                           UserService.get_current_user),
+                       session: Session = Depends(get_session)):
+
+    if current_user.type != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário não autorizado",
+        )
+
+    for id in payment.ids:
+        _user = UserDao(session).get_by_id(id)
+
+        AdminDao(session).create_scolarship_payment(current_user, _user,
+                                                    payment.year,
+                                                    payment.month)
+
+
+@router.post("")
+async def create_finance(expense_log: ExpenseLogIn,
+                         current_user: User = Depends(
+                             UserService.get_current_user),
+                         session: Session = Depends(get_session)):
+
+    if current_user.type != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário não autorizado",
+        )
+
+    _expense = AdminDao(session).create_expense(expense_log, current_user)
 
     if not _expense:
         raise HTTPException(
@@ -61,10 +143,11 @@ def create_finance(expense_log: ExpenseLogIn,
 
 
 @router.put("/{log_id}")
-def update_finance(log_id: int,
-                   expense: ExpenseLogIn,
-                   current_user: User = Depends(UserService.get_current_user),
-                   session: Session = Depends(get_session)):
+async def update_finance(log_id: int,
+                         expense: ExpenseLogIn,
+                         current_user: User = Depends(
+                             UserService.get_current_user),
+                         session: Session = Depends(get_session)):
     if current_user.type != "admin":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
