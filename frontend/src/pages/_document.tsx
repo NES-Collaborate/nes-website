447e8f08b8108aa6fd7b00fc937604407471b8ba
Amon Vanderlei <amon.chalegre@gmail.com>
2024-02-16 19:23:19 -0300

    Deixando o scroll suave

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
+    <Html lang="pt-br" className="scroll-smooth">
       <Head />
       <link rel="icon" href="/favicon.ico" />
       <title>Novo Ensino Suplementar</title>
