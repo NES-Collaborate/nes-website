a08e0f723b3501bd54b157497d87b30d2871d494
sr-pato <oie.eu.sou.um@gmail.com>
2024-02-18 17:56:36 -0300

    Adiciona máscaras para dados no frontend

--- frontend/src/utils/client.ts
+++ frontend/src/utils/client.ts
@@ -43,3 +43,45 @@ export const uploadAttach = async (attach: any, token: string) => {
     return "Erro ao registrar anexo. Tente novamente mais tarde."
   }
 }
+
+// https://github.com/juliossena/vendas-online-web/blob/afd0b010310202cc84c27b73dc6faedf2d5abaa7/src/shared/functions/phone.ts
+export const maskPhone = (phone: string) => {
+  const noMask = phone.replace(/\D/g, "")
+  const { length } = noMask
+  if (length <= 11) {
+    return noMask
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(length === 11 ? /(\d{5})(\d)/ : /(\d{4})(\d)/, "$1-$2")
+  }
+  return phone
+}
+
+export const maskCEP = (cep: string) => {
+  const noMask = cep.replace(/\D/g, "")
+  const formattedCEP = noMask.replace(/(\d{5})(\d{3})/, "$1-$2")
+  return formattedCEP
+}
+
+export const maskCPF = (cpf: string) => {
+  const noMask = cpf.replace(/\D/g, "")
+  const formattedCPF = noMask.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+  return formattedCPF
+}
+
+export const maskMoney = (money: string) => {
+  const cleanedMoney = money.replace(/\D/g, "")
+
+  if (cleanedMoney === "") {
+    return ""
+  }
+
+  const integerPart = cleanedMoney.slice(0, -2)
+  const decimalPart = cleanedMoney.slice(-2)
+
+  // Insert dot every three digits from the end of integer part
+  const formattedInteger = integerPart.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
+
+  const formattedMoney = formattedInteger + "," + decimalPart
+
+  return formattedMoney
+}
