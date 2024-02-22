600bbaeea3e3a37ea8806e5b63ec6a11591e09f5
Amon Vanderlei <amon.chalegre@gmail.com>
2024-02-21 22:00:07 -0300

    Estiliza modal de detalhes de uma despesa

--- frontend/src/components/app/admin/finance/ExpenseLogDetailsModal.tsx
+++ frontend/src/components/app/admin/finance/ExpenseLogDetailsModal.tsx
@@ -1,5 +1,8 @@
 import { EXPENSE_LOG_QUERY_TYPES } from "@/data/translations"
 import { ExpenseLog } from "@/types/finance"
+import clsx from "clsx"
+import Image from "next/image"
+import Link from "next/link"
 import { Button, Modal } from "react-daisyui"
 import { IoMdClose } from "react-icons/io"
 
@@ -29,18 +32,15 @@ const ExpenseLogDetailsModal = ({ isOpen, toggle, logs, expenseId }: Props) => {
         {logs.map((exp) => {
           if (exp.id === expenseId) {
             return (
-              //TODO: Style this modal
               <div className="flex flex-col" key={exp.id}>
-                <h2 className="card-title flex items-center gap-2 text-3xl mb-4">
+                <h2 className="card-title flex items-center gap-2 text-3xl">
                   {exp.category.name}
                 </h2>
+                <p className="mb-4">{exp.comment}</p>
 
-                <div>
+                <div className="flex flex-wrap justify-evenly mb-4">
                   <p>
                     <strong>Adicionado por:</strong> {exp.addedBy.name}
-                  </p>
-                  <p>
-                    <strong>Comentário:</strong> {exp.comment}
                   </p>
                   <p>
                     <strong>Criado em:</strong>{" "}
@@ -50,17 +50,53 @@ const ExpenseLogDetailsModal = ({ isOpen, toggle, logs, expenseId }: Props) => {
                       day: "numeric",
                     })}
                   </p>
+                </div>
 
-                  {/* TODO: Show to whom it was paid and add a button to redirect to its profile */}
-                  {exp.paidTo !== null ? (
-                    <p>
-                      <strong>Pago à:</strong> {exp.paidTo?.name}
-                    </p>
+                <div className="flex flex-wrap justify-evenly">
+                  {exp.paidTo ? (
+                    <div>
+                      <strong>Pago à:</strong>
+
+                      <Link
+                        href={`/app/profile?userId=${exp.paidTo.id}`}
+                        className="p-2 flex flex-col items-center"
+                      >
+                        <Button shape="circle">
+                          {exp.paidTo?.photo?.location && (
+                            <Image
+                              src={exp.paidTo?.photo?.location}
+                              alt={`${exp.paidTo?.name} profile photo`}
+                              width={50}
+                              height={50}
+                              className="rounded-full"
+                            />
+                          )}
+                        </Button>
+                        <span className="text-md text-center">{exp.paidTo?.name}</span>
+                      </Link>
+                    </div>
                   ) : null}
 
-                  <p>
-                    <strong>Comprovante:</strong> exp.proof
-                  </p>
+                  {exp.proof?.location && (
+                    <div className="flex flex-col items-center">
+                      <strong>Comprovante:</strong>
+
+                      <Image
+                        src={exp.proof.location}
+                        alt={`${exp.proof.name} proof`}
+                        width={75}
+                        height={75}
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <div
+                  className={clsx(
+                    exp.type === "Removal" ? "text-error" : "text-success",
+                    "flex flex-col items-end text-2xl"
+                  )}
+                >
                   <p>
                     <strong>Tipo:</strong> {EXPENSE_LOG_QUERY_TYPES[exp.type]}
                   </p>
