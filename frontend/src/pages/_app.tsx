d30d441ea0193f4a2decd9c1bd75af92af4c8077
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-03-07 22:55:48 -0300

    feat: Adicionar QueryClienhtProvider englobando a aplicação

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { BackendProvider } from "@/contexts/backend"
 import { SessionProvider } from "@/contexts/session"
 import { ThemeProvider } from "@/contexts/theme"
 import "@/styles/globals.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import type { AppProps } from "next/app"
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider>
-      <BackendProvider>
-        <ThemeProvider>
-          <DrawerNES>
-            <Component {...pageProps} />
-          </DrawerNES>
-        </ThemeProvider>
-      </BackendProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <BackendProvider>
+          <ThemeProvider>
+            <DrawerNES>
+              <Component {...pageProps} />
+            </DrawerNES>
+          </ThemeProvider>
+        </BackendProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
