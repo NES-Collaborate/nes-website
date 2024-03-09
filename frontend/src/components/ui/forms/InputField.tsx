a1e9e70391a796420d4a5146fd6d05091d6c77d1
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-03-08 21:06:09 -0300

    feat: Implemente fields para componetização de forms

---
+++ frontend/src/components/ui/forms/InputField.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { Tooltip } from "react-daisyui"
+import { FiInfo } from "react-icons/fi"
+
+type InputFieldProps = {
+  label: string
+  mask?: (value: string) => string
+  helpText?: string
+  errors?: {
+    message?: string
+  }
+  onEnter?: (value: string) => void
+}
+
+export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ label, errors, mask = (value) => value, helpText, onEnter, ...rest }, ref) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        e.preventDefault()
+        if (onEnter) {
+          onEnter(e.currentTarget.value)
+          e.currentTarget.value = ""
+        }
+      }
+    }
+
+    return (
+      <div className="form-control w-full max-w-lg">
+        <label className="label">
+          <span className="label-text flex items-center">
+            <span className="mr-1">{label}</span>
+            {helpText && (
+              <Tooltip message={helpText}>
+                <FiInfo />
+              </Tooltip>
+            )}
+          </span>
+        </label>
+        <input
+          ref={ref}
+          className="input input-bordered w-full max-w-lg"
+          {...rest}
+          onChange={(e) => (e.target.value = mask(e.target.value))}
+          onKeyDown={handleKeyDown}
+        />
+        <label className="label">
+          <span className="label-text-alt text-error">{errors?.message}</span>
+        </label>
+      </div>
+    )
+  }
+)
+
+InputField.displayName = "InputField"
