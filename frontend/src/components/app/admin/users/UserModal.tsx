965259535c0061f5f455a73aae7339e3ad207430
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-03-08 21:11:50 -0300

    refactor: Reescreve lógica de implementação do modal de usuário

--- frontend/src/components/app/admin/users/UserModal.tsx
+++ frontend/src/components/app/admin/users/UserModal.tsx
@@ -1,15 +1,48 @@
 import { User } from "@/types/user"
+import { FaTimes } from "react-icons/fa"
+import UserForm from "./UserForm"
 import { ModalState } from "./UsersList"
 
-type Props = {
+export type UserModalProps = {
   user: User | null
   action: "create" | "edit"
   isOpen: boolean
   setModalState: React.Dispatch<React.SetStateAction<ModalState>>
 }
 
-const UserModal = ({ user, action, isOpen, setModalState }: Props) => {
-  return <></>
+const UserModal = ({ user, action, isOpen, setModalState }: UserModalProps) => {
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="user-modal"
+        className="modal-toggle"
+        checked={isOpen}
+        onChange={() => setModalState((prevState) => ({ ...prevState, isOpen: false }))}
+      />
+      <div className="modal" role="dialog">
+        <div className="modal-box w-11/12 max-w-5xl">
+          <button
+            className="btn btn-sm btn-circle bth-ghost absolute right-2 top-2"
+            onClick={() =>
+              setModalState((prevState) => ({
+                ...prevState,
+                user: null,
+                isOpen: false,
+              }))
+            }
+          >
+            <FaTimes />
+          </button>
+          <h3 className="font-bold text-lg">
+            {action === "create" ? "Criar Usuário" : "Editar Usuário"}
+          </h3>
+
+          <UserForm user={user} action={action} setModalState={setModalState} />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default UserModal
