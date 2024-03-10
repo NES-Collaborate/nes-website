c50c149a44a3f54397d06a3c07001e3d766fc224
Felipe Adeildo <felipe.adeildo@proton.me>
2024-03-10 14:48:57 -0300

    feat: Adicionar serviço de notícias

---
+++ frontend/src/services/admin/lp.ts
@@ -0,0 +1,30 @@
+import { Notice } from "@/types/constants"
+import { AxiosInstance } from "axios"
+
+export const fetchNotices = async (client: AxiosInstance): Promise<Notice[]> => {
+  const res = await client.get("/notice/all")
+  return res.data.notices || []
+}
+
+export const deleteNotice = async (
+  client: AxiosInstance,
+  noticeId: number
+): Promise<void> => {
+  await client.delete(`/notice/${noticeId}`)
+}
+
+export const createNotice = async (
+  client: AxiosInstance,
+  notice: Notice
+): Promise<Notice> => {
+  const res = await client.post("/notice/add", notice)
+  return res.data.notice
+}
+
+export const editNotice = async (
+  client: AxiosInstance,
+  notice: Notice
+): Promise<Notice> => {
+  const res = await client.put(`/notice/${notice.id}`, notice)
+  return res.data.notice
+}
