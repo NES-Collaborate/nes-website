1a2d6b3a198d11893e0e89cbcad2f5407363ec5c
Felipe Adeildo <felipe.adeildo@proton.me>
2024-03-10 15:05:15 -0300

    feat: Adiciona wrapper para input do tipo textarea

---
+++ frontend/src/components/ui/forms/TextAreaField.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Tooltip } from "react-daisyui"
+import { FiInfo } from "react-icons/fi"
+
+type TextAreaFieldProps = {
+  label: string
+  helpText?: string
+  errors?: {
+    message?: string
+  }
+  onEnter?: (value: string) => void
+}
+
+export const TextAreaField = React.forwardRef<HTMLTextAreaElement, TextAreaFieldProps>(
+  ({ label, errors, helpText, onEnter, ...rest }, ref) => {
+    return (
+      <div className="form-control w-full max-w-lg">
+        <label className="label">
+          <span className="label-text flex items-center">
+            <span className="mr-1">{label}</span>
+            {helpText && (
+              <Tooltip message={helpText}>
+                <FiInfo />
+              </Tooltip>
+            )}
+          </span>
+        </label>
+        <textarea
+          ref={ref}
+          className="textarea textarea-bordered w-full max-w-lg"
+          {...rest}
+        />
+        <label className="label">
+          <span className="label-text-alt text-error">{errors?.message}</span>
+        </label>
+      </div>
+    )
+  }
+)
+
+TextAreaField.displayName = "TextAreaField"
