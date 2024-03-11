0894fb7fb68063942a35cde19abb34d5b6f69583
Felipe Adeildo <felipe.adeildo@proton.me>
2024-03-10 21:30:24 -0300

    feat: Adiciona hooks de interação com serviços de casos de sucesso

--- frontend/src/hooks/admin/lp.ts
+++ frontend/src/hooks/admin/lp.ts
@@ -1,10 +1,12 @@
 import {
   createNotice,
+  createSuccessCase,
   deleteNotice,
   fetchNotices,
   updateNotice,
+  updateSuccessCase,
 } from "@/services/admin/lp"
-import { Notice } from "@/types/constants"
+import { Notice, SuccessCase } from "@/types/constants"
 import { axiosApi } from "@/utils/axiosClient"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
@@ -41,6 +43,47 @@ export const useNoticeMutations = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["notices"],
+      })
+    },
+  })
+
+  return { deleteMutation, editMutation, createMutation }
+}
+
+export const useSuccessCases = () => {
+  return useQuery<Notice[]>({
+    queryKey: ["success-cases"],
+    queryFn: () => fetchNotices(axiosApi),
+  })
+}
+
+export const useSuccessCasesMutations = () => {
+  const queryClient = useQueryClient()
+
+  const deleteMutation = useMutation({
+    mutationFn: (successCaseId: number) => deleteNotice(axiosApi, successCaseId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["success-cases"],
+      })
+    },
+  })
+
+  const editMutation = useMutation({
+    mutationFn: (successCase: SuccessCase) => updateSuccessCase(axiosApi, successCase),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["success-cases"],
+      })
+    },
+  })
+
+  const createMutation = useMutation({
+    mutationFn: (successCase: Omit<SuccessCase, "id">) =>
+      createSuccessCase(axiosApi, successCase),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["success-cases"],
       })
     },
   })
