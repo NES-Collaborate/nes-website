89a9435270a0114fd7d52b7271575a93b5059826
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-03-11 20:32:33 -0300

    refactor: Implementa `AutoCompleteInput`

--- frontend/src/components/app/admin/property/PropertyForm.tsx
+++ frontend/src/components/app/admin/property/PropertyForm.tsx
@@ -1,9 +1,11 @@
+import { AutoCompleteInput } from "@/components/ui/forms/AutoCompleteInput"
 import { InputField } from "@/components/ui/forms/InputField"
 import { useBackend } from "@/contexts/backend"
 import { useBensMutations } from "@/hooks/admin/bens"
 import { BenFormData, benSchema } from "@/schemas/ben"
 import { fetchUsers } from "@/services/admin/users"
 import { Property } from "@/types/entities"
+import { User } from "@/types/user"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
 import { Button } from "react-daisyui"
@@ -19,17 +21,17 @@ type Props = {
 const PropertyForm = ({ property, action }: Props) => {
   const [isLoading, setIsLoading] = useState(false)
 
-
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<BenFormData>({
     resolver: zodResolver(benSchema),
     values: {
       name: property.name,
       type: property.type,
-      loanedTo: property.loanedTo?.id,
+      loanedTo: property.loanedTo || { name: "" },
     },
   })
 
@@ -39,17 +41,11 @@ const PropertyForm = ({ property, action }: Props) => {
   const submit: SubmitHandler<BenFormData> = async (data) => {
     setIsLoading(true)
 
-    const users = await fetchUsers(backend, "", data.loanedTo)
-    if (!users.length) {
-      return setIsLoading(false)
-    }
-    const user = users[0]
-
     const formData = {
       id: property.id,
       name: data.name,
       type: data.type,
-      loanedTo: user,
+      loanedTo: data.loanedTo as User,
     }
 
     switch (action) {
@@ -64,26 +60,43 @@ const PropertyForm = ({ property, action }: Props) => {
   }
 
   return (
-    <div className="flex flex-col items-center gap-0.5">
+    <div className="flex flex-col items-center gap-4 p-4">
       <InputField label="Nome" {...register("name")} errors={errors.name} />
 
       <InputField
         label="Tipo"
-        helpText="Caso não existe, será criado!"
+        helpText="Caso não exista, será criado!"
         {...register("type")}
         errors={errors.type}
       />
 
-      <InputField
-        helpText="UserID aqui"
-        label="Emprestado a"
+      <AutoCompleteInput
+        label="Emprestado para"
+        helpText="Pessoa a quem foi emprestado"
         {...register("loanedTo")}
+        fetchSuggestions={async (query) =>
+          (await fetchUsers(backend, query)).map((u) => ({
+            label: u.name,
+            value: u.id.toString(),
+          }))
+        }
+        onSuggestionSelect={(suggestion) => {
+          setValue(
+            "loanedTo",
+            {
+              id: parseInt(suggestion.value),
+              name: suggestion.label,
+            },
+            { shouldValidate: true }
+          )
+        }}
         errors={errors.loanedTo}
       />
 
       <Button
         variant="outline"
         color="accent"
+        className="flex items-center gap-2"
         onClick={handleSubmit(submit)}
         disabled={isLoading}
       >
