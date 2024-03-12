7b8cb769333ae64b2190a90bfea11f403c3ca3fd
Felipe Adeildo <oie.eu.sou.um@gmail.com>
2024-03-11 22:13:53 -0300

    refactor: Implementa hook `useFinanceStats` no lugar do SSR

--- frontend/src/pages/app/admin/finance/index.tsx
+++ frontend/src/pages/app/admin/finance/index.tsx
@@ -2,21 +2,15 @@ import ExpenseLogStats from "@/components/app/admin/finance/ExpenseLogStats"
 import ExpenseLogs from "@/components/app/admin/finance/ExpenseLogs"
 import { ExpenseLogsProvider } from "@/contexts/expenseLogs"
 import { withAuth } from "@/utils/auth"
-import { axiosServer } from "@/utils/axiosClient"
 
-type FinanceProps = {
-  currentBalance: number
-  totalExpenses: number
-}
-
-const Finance = (props: FinanceProps) => {
+const Finance = () => {
   return (
     <>
       <h1 className="text-2xl text-center my-3 font-semibold">Gestão Financeira</h1>
 
       <div className="container mx-auto w-11/12">
         <ExpenseLogsProvider>
-          <ExpenseLogStats {...props} />
+          <ExpenseLogStats />
           <ExpenseLogs />
         </ExpenseLogsProvider>
       </div>
@@ -27,26 +21,5 @@ const Finance = (props: FinanceProps) => {
 export default Finance
 
 export const getServerSideProps = withAuth({
-  callback: async ({ req }) => {
-    try {
-      const res = await axiosServer.get("/admin/finance/stats", {
-        headers: {
-          Authorization: `Bearer ${req.cookies._token}`,
-        },
-      })
-
-      return {
-        props: res.data,
-      }
-    } catch {
-      return {
-        redirect: {
-          destination: "/app/admin",
-          permanent: false,
-        },
-      }
-    }
-  },
-
   allowedUsers: ["admin"],
 })
