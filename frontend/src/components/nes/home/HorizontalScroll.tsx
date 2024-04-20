080612990f9e50468fc77fef560cc4c7865068bc
Amon Vanderlei <amon.chalegre@gmail.com>
2024-04-20 10:37:06 -0300

    feat: Adiciona Horizontal Scroll às notícias

---
+++ frontend/src/components/nes/home/HorizontalScroll.tsx
@@ -0,0 +1,38 @@
+import { Notice } from "@/types/constants"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useRef } from "react"
+import NoticeCard from "./NoticeCard"
+
+type Props = {
+  notices: Notice[]
+}
+
+const HorizontalScroll = ({ notices }: Props) => {
+  const targetRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  })
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"])
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900 w-full">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-4">
+          {notices.map((notice, i) => {
+            return (
+              <div
+                key={i}
+                className="group transform transition-transform hover:scale-105"
+              >
+                <NoticeCard notice={notice} />
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
+    </section>
+  )
+}
+
+export default HorizontalScroll
--- frontend/src/components/nes/home/Notices.tsx
+++ frontend/src/components/nes/home/Notices.tsx
@@ -1,6 +1,6 @@
 import Loading from "@/components/Loading"
 import { useNotices } from "@/hooks/admin/lp"
-import NoticeCard from "./NoticeCard"
+import HorizontalScroll from "./HorizontalScroll"
 
 /**
  * Get a notice lists from backend and show them.
@@ -16,19 +16,13 @@ const Notices = () => {
   return (
     <div className="w-full flex flex-col items-center mb-4">
       <h1 className="text-primary text-2xl stroke-1 my-8">Notícias</h1>
-      <div className="flex flex-nowrap gap-3 overflow-x-scroll w-11/12 py-10">
+      <div className="flex flex-nowrap gap-3 w-11/12">
         {isLoading && (
           <div className="w-full">
             <Loading text="Carregando notícias..." textClassName="text-lg" center />
           </div>
         )}
-        {notices.map((notice, i) => {
-          return (
-            <div key={i} className="group transform transition-transform hover:scale-105">
-              <NoticeCard notice={notice} />
-            </div>
-          )
-        })}
+        <HorizontalScroll notices={notices} />
       </div>
     </div>
   )
