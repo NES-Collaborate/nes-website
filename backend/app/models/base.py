5d66ddeaa7f45c61327cf256b6381b18966f8ac2
ThiagoORuby <trs@ic.ufal.br>
2024-04-20 20:50:56 -0300

    Adiciona AddedByMixin a BaseTable

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -2,14 +2,21 @@ from datetime import datetime, timezone
 from typing import Annotated, Optional
 
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-class BaseTable(DeclarativeBase):
+class AddedByMixin(object):
+
+    addedById: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("users.id"))
+    addedBy = relationship("User")
+
+
+class BaseTable(AddedByMixin, DeclarativeBase):
 
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     createdAt: Mapped[Optional[datetime]] = mapped_column(
-        sa.DateTime, default=lambda: datetime.now(timezone.utc))
+        sa.DateTime, default=lambda: datetime.now(timezone.utc)
+    )
     updatedAt: Mapped[Optional[datetime]] = mapped_column(sa.DateTime)
 
 
