7fbcb16471f6ccd70101f474792851e8d799ee1e
Felipe Adeildo <felipe.adeildo0@gmail.com>
2024-06-25 10:49:10 -0300

    fix: Múltiplas requisições para pesquisa query de posts

--- frontend/src/components/classroom/PostsFilter.tsx
+++ frontend/src/components/classroom/PostsFilter.tsx
@@ -1,22 +1,20 @@
 import { Button, Input, Tooltip } from "react-daisyui"
 import { FaSearch } from "react-icons/fa"
-import { useDebounceCallback } from "usehooks-ts"
+import { DebouncedState } from "usehooks-ts"
 
 type Props = {
-  query: [string, React.Dispatch<React.SetStateAction<string>>]
+  query: [string, DebouncedState<(value: string) => void>]
   isFetching: boolean
 }
 
-const PostsFilter = ({ query: [query, setQuery], isFetching }: Props) => {
-  const debouncedSearch = useDebounceCallback(setQuery, 500)
-
+const PostsFilter = ({ query: [_, setQuery], isFetching }: Props) => {
   return (
     <div className="join flex justify-center mb-8 gap-1">
       <Input
         size="sm"
         placeholder="Pesquisar"
         className="w-3/4 lg:w-2/4 join-item"
-        onKeyDown={(e) => debouncedSearch(e.currentTarget.value)}
+        onKeyDown={(e) => setQuery(e.currentTarget.value)}
       />
       <Tooltip message="Pesquisar">
         <Button size="sm" color="primary" disabled={isFetching} className="join-item">
