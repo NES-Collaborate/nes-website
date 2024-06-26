9270767fcd47007d11a9a0cd5edd5e2787562152
Edeilson <edeilsonazevedo22@gmail.com>
2024-06-26 13:52:08 -0300

    test: forma codigo e retira test mal elaborado

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,45 +1,31 @@
 from fastapi.testclient import TestClient
-from .utils import login_and_get_token
-
-# Teste para verificar se a API de login retorna um status 200 para credenciais válidas
-def test_login_success(client: TestClient):
-    response = client.post(
-        "/api/login", data={"username": "00000000000", "password": "admin"}
-    )
-    assert response.status_code == 200
 
 
-# Teste para assegurar que o sistema rejeita logins com uma senha incorreta
-def test_login_wrong(client: TestClient):
-    response = client.post(
-        "api/login", data={"username": "00000000000", "password": "wrong"}
-    )
-    assert response.status_code == 400
-    assert response.json() == {"detail": "CPF ou senha incorretos"}
-
-
-# Teste para verificar se a API de logout retorna um status 200
-def test_logout(client: TestClient):
-    response = client.get("/api/logout")
-    assert response.status_code == 200
-    assert response.json() == {"message": "logout realizado"}
-
-
-# Teste para verificar se o busca de um user por cpf retorna um status 200
-def test_get_user_success(client: TestClient):
-    # Passo 1: Login e obtenção do token
+def test_login_with_wrong_credentials_returns_error(client: TestClient):
+    """
+    Verifica se o login falha quando são fornecidas credenciais incorretas.
     
-    token = login_and_get_token(client, "00000000000", "admin")
-    # Passo 2: Acessar a rota /me com o token de autenticação
-    headers = {"Authorization": f"Bearer {token}"}
-    response = client.get("/api/me", headers=headers)
-    user_data = response.json()
-    # Passo 3: Verificações finais
-    assert response.status_code == 200
-    assert user_data["name"] == "Admin"
-    assert user_data["cpf"] == "00000000000"
-    assert user_data["type"] == "admin"
+    Este teste verifica se o sistema retorna um status 400 e a mensagem 
+    de erro apropriada quando um usuário tenta fazer login com um CPF 
+    e senha incorretos.
+    """
+    response = client.post("api/login",
+                           data={
+                               "username": "00000000000",
+                               "password": "wrong"
+                           })
+    assert response.status_code == 400
+    assert response.json()["detail"] == "CPF ou senha incorretos"
 
-    # Verificar se os campos relacionais existem (exemplo: 'emails', 'phones')
-    assert "emails" in user_data
-    assert "phones" in user_data
+
+
+def test_logout_with_credentials_return_success(client_with_token: TestClient):
+    """" 
+    Verifica se o logout funciona com credenciais corretas.
+    
+    Este teste verifica se o sistema retorna um status 200 e a mensagem 
+    de sucesso apropriada quando um usuário faz logout.
+    """
+    response = client_with_token.get("/api/logout")
+    assert response.status_code == 200
+    assert response.json()["message"] ==  "logout realizado"
