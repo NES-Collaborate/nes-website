8d1da9c95903fad6bc22e11ba93c62728e563598
Edeilson <edeilsonazevedo22@gmail.com>
2024-06-26 13:49:03 -0300

    test: adiciona nova fixture, client_with_toke para simplificar os tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,18 @@
 import pytest
 from app import app
 from fastapi.testclient import TestClient
+from tests.utils import login_and_get_token
 
 
 @pytest.fixture(scope="function")
 def client():
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture(scope="function")
+def client_with_token(client: TestClient):
+    token = login_and_get_token(client, "00000000000", "admin")
+    headers = {"Authorization": f"Bearer {token}"}
+    client.headers = headers
+    return client
