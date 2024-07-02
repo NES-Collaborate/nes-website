65882a28f2181ff87991740e9d91cd0736228da5
Luanderson Souza <70651073+Luandersonsouza@users.noreply.github.com>
2024-07-02 11:34:45 -0300

    Update comment.py
    changes are finished, trust in the dad

--- backend/app/routers/comment.py
+++ backend/app/routers/comment.py
@@ -9,6 +9,65 @@ from app.services.user import UserService
 
 router = APIRouter(prefix="/comment", tags=["comment"])
 
+@router.get("/{postId}/comment", status_code=status.HTTP_200_OK)
+@paginated_response
+async def get_all_comment(
+    postId: int,
+    p: int = Query(1, ge=1),
+    pp: int = Query(10, ge=10, le=50),
+    current_user: User = Depends(UserService.get_current_user),
+    session: Session = Depends(get_session),
+):
+    if current_user.type != "student":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário não autorizado",
+        )
+
+    dao = GeneralDao(session)
+
+    comments, _ = dao.get_comments(postId=postId, page=p, page_size=pp)
+
+    comments_out = [
+        CommentOut(
+            id=comment.id,
+            content=comment.content,
+            addedBy=Author(name=comment.addedBy.name, id=comment.addedBy.id),
+            createdAt=comment.createdAt,
+        )
+        for comment in comments
+    ]
+
+    return [CommentOut.model_validate(comment) for comment in comments_out]
+
+
+@router.post("/{postId}/comment", status_code=status.HTTP_201_CREATED)
+async def add_comment(
+    postId: int,
+    comment: CommentInp,
+    current_user: User = Depends(UserService.get_current_user),
+    session: Session = Depends(get_session),
+):
+    if current_user.type != "student":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário não autorizado",
+        )
+
+    author = Author(name=current_user.name, id=current_user.id)
+    dao = GeneralDao(session)
+    comment_content, comment_id, createdAt = dao.create_comment(
+        comment=comment, postId=postId, author=author
+    )
+
+    response = CommentOut(
+        id=comment_id,
+        content=comment_content,
+        addedBy=author,
+        createdAt=createdAt,
+    )
+
+    return response
 
 @router.put(
     "/{commentId}",
